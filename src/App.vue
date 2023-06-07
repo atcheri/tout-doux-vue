d442c4c606046273da316cc799bdfedf7a1fc08e
Atcheri Atch <atcheri@gmail.com>
2023-06-07 14:42:55 +0200

    feat: add todo list component
    As well as some custom style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ToDoList from "./components/ToDoList.vue";
+</script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Tout-Doux List</h1>
-  </div>
+  <main class="container mx-auto h-screen">
+    <section class="flex flex-col justify-center gap-4 items-center p-2 md:p-6">
+      <a href="/">
+        <img src="/logo.svg" class="h-16 w-16" alt="Tout-Doux logo" />
+      </a>
+      <h1 class="text-3xl font-bold">Tout-Doux List</h1>
+    </section>
+    <ToDoList />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.logo {
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
